06e2e4b6956515d66f4c2942e99e7c2e5773a58f
kilbergr <rekilberg@gmail.com>
2015-08-12 23:13:45 -0700

    altering display to avoid td with ng-show

--- redditClone/app.js
+++ redditClone/app.js
@@ -3,19 +3,24 @@ app.controller("postController", function($scope){
 	$scope.posts = new Array();
 
 	$scope.addPost = function(){
-		$scope.posts.push({
-			title: $scope.post.title,
-			content: $scope.post.content,
-			image: $scope.post.image,
-			date: new Date(),
-			comments: new Array(),
-			showCommentForm: false
-		})
-		$scope.post.title = '';
-		$scope.post.content ='';
-		$scope.post.image ='';
-		$scope.post.date = '';
+		if ($scope.post.title && $scope.post.content && $scope.post.image){
+			$scope.posts.push({
+				title: $scope.post.title,
+				content: $scope.post.content,
+				image: $scope.post.image,
+				date: new Date(),
+				comments: new Array(),
+				showCommentForm: false,
+				vote: 0
+			})
+			$scope.post.title = '';
+			$scope.post.content ='';
+			$scope.post.image ='';
+			$scope.post.date = '';
+		}
+		else alert("Please fill in all form fields!");
 	}
+			
 
 	// $scope.post.comments = new Array();
 
@@ -29,4 +34,6 @@ app.controller("postController", function($scope){
 	}
 
 })
+
+
 
